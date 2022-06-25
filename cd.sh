eb139015e2a7afb9416421ba2180b6840e4b592b
dito <dimitrikvirik@gmail.com>
2022-06-25 19:56:37 +0400

    test

--- cd.sh
+++ cd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-git pull
-docker-compose -f docker-services/docker-compose.yml stop -d anychat
-mvn spring-boot:build-image
-docker-compose -f docker-services/docker-compose.yml up -d anychat
+echo "Hello $USER"
+echo "Today is $(date)"
+echo "Bye for now"
+#git pull
+#docker-compose -f docker-services/docker-compose.yml stop -d anychat
+#mvn spring-boot:build-image
+#docker-compose -f docker-services/docker-compose.yml up -d anychat
