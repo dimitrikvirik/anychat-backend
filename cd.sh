bee8bfff4702a0b9849ca9c2a7ff37f9ee6c7994
dito <dimitrikvirik@gmail.com>
2022-06-25 19:57:39 +0400

    test

--- cd.sh
+++ cd.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo "Hello $USER"
-echo "Today is $(date)"
-echo "Bye for now"
-#git pull
-#docker-compose -f docker-services/docker-compose.yml stop -d anychat
-#mvn spring-boot:build-image
-#docker-compose -f docker-services/docker-compose.yml up -d anychat
+git pull
+docker-compose -f docker-services/docker-compose.yml stop -d anychat
+mvn spring-boot:build-image
+docker-compose -f docker-services/docker-compose.yml up -d anychat
