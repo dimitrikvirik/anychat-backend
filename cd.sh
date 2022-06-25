2808fb14c86a5bf935ef55bb6a08ca315e030584
dito <dimitrikvirik@gmail.com>
2022-06-25 20:14:38 +0400

    updated bash script

--- cd.sh
+++ cd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git pull
+git checkout master
+git pull origin master
 docker-compose -f docker-services/docker-compose.yml stop anychat
 mvn spring-boot:build-image
 docker-compose -f docker-services/docker-compose.yml up -d anychat
