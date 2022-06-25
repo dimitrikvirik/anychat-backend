8e42e1b0747e5280ef13bbcdea27aa6bfece7c6a
dito <dimitrikvirik@gmail.com>
2022-06-25 19:44:55 +0400

    removed mustNotBeBlack

--- src/main/java/git/dimitrikvirik/anychatbackend/model/param/UserUpdateParam.java
+++ src/main/java/git/dimitrikvirik/anychatbackend/model/param/UserUpdateParam.java
@@ -8,12 +8,10 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class UserUpdateParam {
 
-    @Length(min = 3, max = 30)
-    @NotBlank
+
     private String Firstname;
 
-    @Length(min = 3, max = 30)
-    @NotBlank
+
     private String Lastname;
 
     private String About;
