2e7df8771392ba4e9cd845119fba3d6ed0a72e13
dito <dimitrikvirik@gmail.com>
2022-06-26 23:23:35 +0400

    fixed photo dir

--- src/main/java/git/dimitrikvirik/anychatbackend/AnychatBackendApplication.java
+++ src/main/java/git/dimitrikvirik/anychatbackend/AnychatBackendApplication.java
@@ -5,12 +5,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 
 public class AnychatBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnychatBackendApplication.class, args);
+        try {
+            Files.createDirectories(Paths.get("photos"));
+        }catch (IOException e){
+
+        }
     }
 
 }
